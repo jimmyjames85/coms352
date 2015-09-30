83c20aca4235199261dfb1a20aa4b014f4136f11
U-MSC\jtappe <jtappe@AMS-JTAPPE3.MSC.local>
2015-09-30 14:48:10 -0500

    cmd loop

--- shell/main.c
+++ shell/main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
+#include <unistd.h>
+
+#define INPUT_STRING_BUFFER_SIZE 256
 
 int main(int argc, char * argv[])
 {
-  int i;
-  printf("Hello World!\n");
+     /*  int i;
+	 for(i=0; i < argc ;i++)
+	 {
+	 printf("Argument %i: %s\n", i,  argv[i]);
+	 }*/
 
-  for(i=0; i < argc ;i++)
-  {
-    printf("Argument %i: %s\n", i,  argv[i]);
-  }
+     char * inputString = ((char *)malloc((sizeof(char)*INPUT_STRING_BUFFER_SIZE)));
+     *inputString = '\0';
+     
+     fgets(inputString, INPUT_STRING_BUFFER_SIZE-1, stdin);
+     printf("You entered: %s\r\n",inputString);
+
+     fflush(stdin);
+     free((void *)inputString);
+     return 0;
 }
