3f97c5347266158f1feb1a9293f89cfa7725764c
jmt <jmt>
2015-10-02 00:17:57 -0500

    funky behavior right now

--- shell/main.c
+++ shell/main.c
@@ -1,31 +1,295 @@
-#include <stdio.h>    /// Includes FILE typedef, NULL 
-
-//#include <unistd.h> <---- replaces syscalls.h????
-#include <ctype.h> //Char type
+#include <stdio.h>      /* printf, scanf, NULL, FILE , stdin, stdout, stderr*/
+#include <stdlib.h>     /* malloc, free, rand */
+#include <string.h>  /* strstr strcmp*/
+#include <unistd.h> /* pid_t fork */
+#include <ctype.h> /*Char type*/
 
 #include <stdarg.h> /* standard header contains a set of macro
 		     * definitions that define how to step through an
 		     * argument list. */
 
-#define INPUT_STRING_BUFFER_SIZE 256
+#define CR 0x0d
+#define LF 0x0a
+#define CLI "jsh$ "
 
 
+/**
+ * Argument node used to make a linked list of arguments for a command
+ */
+typedef struct anode
+{
+     char * arg;
+     struct anode *next;
+} anode;
 
+void debug(char * msg)
+{
+     printf("%s",msg );
+}
+
+/**
+ * Creates and returns an anode with arg and next both pointing to
+ * NULL. If malloc fails then it returns NULL;
+ */
+anode *new_anode()
+{
+     
+     debug("creating new node...\n");
+     anode *newanode = (anode *) malloc(sizeof(anode)); 
+     if(newanode!=NULL)
+     {
+	  newanode->arg = NULL;
+	  newanode->next = NULL;
+	  debug("initializing new node...\n");
+     }
+     else
+	  debug("returning NULL node\n");
+     return newanode;
+}
+char * eatWhiteSpace(char * s)
+{ 
+     while((*s==' ') && (*s!='\0'))
+	  s++;
+     return s; 
+}
+
+/**
+ * Creates and returns an anode linked list of arguments from the
+ * c-string cmd. The linked list is terminated by a NULL pointer. 
+ * 
+ * This function modifies the cmd string by replacing all spaces
+ * outside of quotes with the terminator '\0'. The returned linked
+ * list contains pointers to the begining of each subsequent cmd.
+ *
+ */
+anode *arg_to_linked_list(char * cmd)
+{
+     char inside=0; /* determines if we are inside a quote(1) or a cmd(2)"*/
+     char *c = cmd; /* the current character we are processing */   
+     anode *head = new_anode();
+     if(head==NULL) return head; /* failure */
+
+     anode *tail = head;
+
+
+     c = eatWhiteSpace(c);
+     char *ab = c; /* argument begining */
+
+     while(*c != '\0')
+     {
+	  c = eatWhiteSpace(c);
+	  ab = c;
+	  printf("   %s\r\n",ab);
+	  
+	  if(*c=='"')
+	  {
+	       c++;
+	       ab = c; /* after the quote */
+	       while(*c!='"' && *c!='\0')
+		    c++;
+	       /*c points to end of quoted argument */
+	  }
+	  else
+	  {
+	       int i=0;
+	       while((*c)!=' ' && (*c)!='\0')
+	       {
+		    printf("%d:%s\n",i++,c);
+		    printf("%c\n",*c);
+		    c++;
+		    printf("'%c'\n",*c);
+	       }
+	       
+
+	       printf("exit\n");
+	       /*c points to end of unquoted argument */
+	  }
+	  printf("exited\n");
+	  printf("exiteda\n");
+
+	  if((*c) != '\0')
+	  {
+	       printf("exitedb");
+
+	       /* *c='\0'; 
+	       printf("2");
+	       c++;
+	       printf("3");*/
+	  }
+
+	  printf("Extracted: '%s' to '%s'\n",ab,c);
+	  printf("4");
+	  tail->next = new_anode();
+	  if(tail->next==NULL)
+	  {
+	       printf("NULLERROR");
+	       /* TODO dealloc the rest of the anodes */
+	       return head; /* failure */
+	  }
+	  tail->arg = ab;
+	  
+	  tail = tail->next;
+
+
+     }
+     return head;
+}
+
+/**
+ * Reads in a line from a file until it reaches a CR,LF, or a CRLF.
+ * Removes the CR,LF,or CRLF and replaces it with '\0'
+ *
+ * FILE* fstream 	file to read from (assumed to be open)
+ * char **line 		address of a pointer to a char (where to put the string)
+ *
+ * uses malloc to allocate memory at *line
+ * DOES NOT FREE MEMORY if successfull
+ *
+ *
+ *	returns  0 if succesful
+ *
+ *	returns -2 if malloc or realloc could not allocate memory
+ * DOES NOT FREE MEMORY allocated to **line if function ends successfully
+ * */
+int fpreadl(FILE* fstream, char** line)
+{
+
+/*Start with 1 byte and if we need more then we double this each time*/
+     unsigned totBytes = 1;
+     *line = (char *) malloc(totBytes * sizeof(char));
+     if ((*line) == NULL)
+	  return -2;
+
+     int endOfLine = 0;
+     int pos = 0;
+     while (!feof(fstream) && !endOfLine)
+     {
+	  if (pos > (totBytes >> 2))
+	  {
+	       /*Then we need to reallocate some memory.
+		* So we double the amount we need.*/
+	       totBytes *= 2;
+	       char* tmp = (char*) realloc(*line, totBytes * sizeof(char));
+	       /*if reallocation fails*/
+	       if (tmp == NULL)
+	       {
+		    /*free what we have and exit*/
+		    free(*line);
+		    return -2;
+	       }
+	       /*otherwise lets continue*/
+	       *line = tmp;
+	  }
+
+	  /*readin our next char*/
+	  (*line)[pos] = fgetc(fstream);
+	  /*check for endOfLine*/
+	  switch ((*line)[pos])
+	  {
+	  case CR:
+	  {
+	       /*Check for WINDOWS CRLF*/
+	       if (!feof(fstream))
+	       {
+		    (*line)[pos] = fgetc(fstream);
+		    /*If it is not a lineFeed then put it back*/
+		    if ((*line)[pos] != LF)
+			 ungetc((*line)[pos], fstream);
+	       }
+	       endOfLine = 1;
+	       break;
+	  }
+	  case LF:
+	  {
+	       /*if we have CR or CRLF or LF set endOfLine*/
+	       endOfLine = 1;
+	       break;
+	  }
+	  }
+	  pos++;
+     }
+
+/** If this while loop was successful then pos points to 1 past the last character that we read in
+ *  AND the last character we read in was either a CR or a LF so we need to decrement pos and
+ *  change that last character to the null character \0. However if the while loop was never entered
+ *  then pos=0 and we set that character to \0 and return an empty string.*/
+
+     if (endOfLine || feof(fstream))
+	  pos--;
+     (*line)[pos] = '\0';
+
+     /*success!*/
+     return 0;
+}
+
+int readl(char** line)
+{
+     return fpreadl(stdin, line);
+}
 
 
 int main(int argc, char * argv[])
 {
 
-     void sayHello(void)
+     char * cmd;
+     char keepGoing = 1;
+     char isChild = 0;
+
+     while( keepGoing )
      {
-	  printf("Hello!\r\n");
-     };
-     /*  int i;
-	 for(i=0; i < argc ;i++)
-	 {
-	 printf("Argument %i: %s\n", i,  argv[i]);
-	 }*/
-     void tryToSayHello(void);     
+	  if(isChild)
+	       printf("CHILD$ ");
+	  else
+	       printf("%s",CLI);
+
+	  if( readl(&cmd) ) 
+	  {
+	       keepGoing=0;
+	       continue;
+	  }
+	  if(strstr(cmd, "cmd")==cmd)	  
+	  {
+	       //TODO test cmd_to
+	       anode *list = arg_to_linked_list("     hello there sir this is a list");
+	       anode *cur = list;
+	       while(cur!=NULL)
+	       {
+		    printf("%s ", cur->arg);
+		    cur = cur->next;
+	       }
+	       printf("\r\nList complete.\r\n");
+	  }
+	  else if(strstr(cmd, "fork")==cmd)	  
+	  {
+	       pid_t p = fork();
+	       if(p == -1) /* TODO handle error*/
+		    printf("Unable to fork\r\n");
+	       else if(p==0) /* child */
+	       {
+		    isChild=1;
+		    printf("I'm a new child\r\n");
+		    char * argv[]={"hi",NULL};
+	       }
+	       else /* parent */
+	       {
+		    printf("Waiting for pid(%d) to finish...\r\n",p);
+		    int status;
+		    waitpid(p, &status, 0);
+		    printf("pid(%d) finished with status=%d\r\n",p,status);
+	       }
+	  }
+	  else if(strstr(cmd, "exit")==cmd)
+	  {
+	       keepGoing=0;
+	       printf("Goodbye.\r\n");
+	  }
+	  else
+	       printf("You typed: %s\r\n", cmd);
+
+	  free(cmd);
+
+     }
+
 /*
   char * inputString = ((char *)malloc((sizeof(char)*INPUT_STRING_BUFFER_SIZE)));
   *inputString = '\0';
@@ -37,19 +301,9 @@ int main(int argc, char * argv[])
   free((void *)inputString);
   return 0;
 */
-     sayHello();
-     tryToSayHello();
-     char c;
-     while((c = getchar()) != EOF )
-	  putchar(tolower(c));
+
 
 
      return 0;
 }
 
-
-
-void tryToSayHello(void)
-{
-     sayHello();
-}
